2662158b0a4a5c7c2eaad43b294422c78dd796d9
Badlylucky <u770518g@ecs.osaka-u.ac.jp>
2021-12-08 15:18:33 +0900

    IPO_Hを実装

---
+++ IPOsolver/solver.cpp
@@ -0,0 +1,153 @@
+#include<iostream>
+#include<vector>
+#include<utility>
+#include<set>
+#include<algorithm>
+#include<iterator>
+using namespace std;
+
+vector<vector<int> > ans;
+int t=2;
+int k;
+pair<int,int> v[20];
+int popcount(unsigned int n){
+    int ret=0;
+    for(unsigned int i=0;i<32;i++){
+        if(n&(1<<i)){
+            ret++;
+        }
+    }
+    return ret;
+}
+void outputline(vector<int> &line){
+    for(int i=0;i<line.size();i++){
+        cout<<line[i];
+        if(i<line.size()-1){
+            cout<<',';
+        }else{
+            cout<<endl;
+        }
+    }
+    return;
+}
+//最初のt個分の組み合わせを決定し、テストケースを生成する
+void initGenerate(int cnt, vector<int> val){
+    if(cnt==0){
+        vector<int> tmp(k,-1);
+        for(int i=0;i<val.size();i++){
+            tmp[v[i].second]=val[i];
+        }
+        // for(int i=0;i<tmp.size();i++){
+        //     cerr<<tmp[i]<<",";
+        // }
+        // cerr<<endl;
+        ans.push_back(tmp);
+        return;
+    }else{
+        for(int i=0;i<v[t-cnt].first;i++){
+            vector<int> cpy;
+            copy(val.begin(),val.end(),back_inserter(cpy));
+            cpy.push_back(i);
+            initGenerate(cnt-1,cpy);
+        }
+    }
+}
+// Algorithm: IPO_Hを実行する
+void IPOH(int ind){
+    // interactionのうちindが絡むものを全て列挙する
+    vector<set<pair<int, int> > > interactions(v[ind].first);
+    vector<int> target;
+    // 現在のところt=2固定なのでbit全探索の意味はない
+    for(int i=0;i<(1<<t);i++){
+        if(popcount(i)==t-1){
+            cerr<<i<<endl;
+            target.push_back(i);
+        }
+    }
+    for(int param=0;param<v[ind].first;param++){
+        // vector<pair<int,int>> oneinteraction;
+        for(int i=0;i<target.size();i++){
+            for(int bit=0;bit<t;bit++){
+                if(target[i]&(1<<bit)){
+                    for(int j=0;j<v[bit].first;j++){
+                        interactions[param].insert(make_pair(v[bit].second, j));
+                    }
+                }
+            }
+        }
+        cerr<<interactions[param].size()<<endl;
+    }
+    cerr<<"kazoeage done"<<endl;
+    // 最初のv[ind]個は順番に割り付けを行う
+    for(int i=0;i<v[ind].first;i++){
+        ans[i][v[ind].second]=i;
+        // 満たされたinteractionの削除を行う
+        for(int j=0;j<k;j++){
+            if(ans[i][j]!=-1 && interactions[i].count(make_pair(j, ans[i][j]))){
+                interactions[i].erase(make_pair(j, ans[i][j]));
+            }
+        }
+    }
+    cerr<<"first edit done"<<endl;
+    // それ以降は貪欲に割り付ける
+    for(int i=v[ind].first;i<ans.size();i++){
+        cerr<<i<<endl;
+        int maxparam=-1;
+        int maxcnt=-1;
+        // パラメータ全てについて関連するinteractionを列挙
+        for(int j=0;j<v[ind].first;j++){
+            int cnt=0;
+            for(int l=0;l<k;l++){
+                if(ans[i][l]!=-1 && interactions[j].count(make_pair(l, ans[i][l]))){
+                    cnt++;
+                }
+            }
+            // cerr<<cnt<<endl;
+            if(cnt>maxcnt){
+                maxcnt=cnt;
+                maxparam=j;
+            }
+        }
+        // 決まった値で更新する
+        ans[i][v[ind].second]=maxparam;
+        cerr<<"maxparam done"<<endl;
+        // 満たされたinteractionの削除を行う
+        for(int j=0;j<k;j++){
+            if(ans[i][j]!=-1 && interactions[maxparam].count(make_pair(j, ans[i][j]))){
+                interactions[maxparam].erase(make_pair(j, ans[i][j]));
+            }
+        }
+    }
+    cerr<<"second edit done"<<endl;
+}
+void solve(){
+    // count all interactions
+    sort(v,v+k);
+    reverse(v,v+k);
+    // 大きいものt個の組み合わせを全て割り当てる
+    initGenerate(t, vector<int>());
+    // for(int i=0;i<ans.size();i++){
+    //     outputline(ans[i]);
+    // }
+    cerr<<"init generate done"<<endl;
+    // 残りのk-t個について調べる
+    for(int i=t;i<k;i++){
+        IPOH(i);
+    }
+}
+int main(){
+    // input
+    cin>>t>>k;
+    for(int i=0;i<k;i++){
+        int tmp;
+        cin>>tmp;
+        v[i]=make_pair(tmp,i);
+    }
+    solve();
+    // output
+    cout<<ans.size()<<endl;
+    for(int i=0;i<ans.size();i++){
+        outputline(ans[i]);
+    }
+    return 0;
+}
